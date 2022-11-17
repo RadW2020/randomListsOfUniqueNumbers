ca4cbf8efc484b276e5363860d98447418dec86e
RadW2020 <RadW2020@users.noreply.github.com>
2022-11-17 22:44:22 +0100

    Update firstTry.ts

--- firstTry.ts
+++ firstTry.ts
@@ -1,64 +1,6 @@
 let listaCorreo = [
-'36591 -1',
-'36591 -2',
-'36591 -3',
-'36592 -1',
-'36592 -2',
-'36592 -3',
-'36593 -1',
-'36593 -2',
-'36593 -3',
-'36594 -1',
-'36594 -2',
-'36594 -3',
-'36595 -1',
-'36595 -2',
-'36595 -3',
-'36596 -1',
-'36596 -2',
-'36596 -3',
-'36597 -1',
-'36597 -2',
-'36597 -3',
-'36598 -1',
-'36598 -2',
-'36598 -3',
-'36599 -1',
-'36599 -2',
-'36599 -3',
-'36600 -1',
-'36600 -2',
-'36600 -3',
-'36601 -1',
-'36601 -2',
-'36601 -3',
-'36602 -1',
-'36602 -2',
-'36602 -3',
-'36603 -1',
-'36603 -2',
-'36604 -1',
-'36604 -2',
-'36605 -1',
-'36605 -2',
-'36606 -1',
-'36606 -2',
-'36607 -1',
-'36607 -2',
-'36608 -1',
-'36608 -2',
-'36609 -1',
-'36609 -2',
-'36610 -1',
-'36610 -2',
-'36611 -1',
-'36611 -2',
-'36612 -1',
-'36612 -2',
-'36613 -1',
-'36613 -2',
-'36614 -1',
-'36614 -2'
+'36591 -1','36591 -2','36591 -3','36592 -1','36592 -2','36592 -3','36593 -1','36593 -2','36593 -3','36594 -1','36594 -2','36594 -3','36595 -1','36595 -2','36595 -3','36596 -1','36596 -2','36596 -3','36597 -1','36597 -2','36597 -3','36598 -1','36598 -2','36598 -3','36599 -1','36599 -2','36599 -3','36600 -1','36600 -2','36600 -3','36601 -1','36601 -2','36601 -3','36602 -1','36602 -2','36602 -3','36603 -1','36603 -2','36604 -1','36604 -2','36605 -1','36605 -2','36606 -1',
+'36606 -2','36607 -1','36607 -2','36608 -1','36608 -2','36609 -1','36609 -2','36610 -1','36610 -2','36611 -1','36611 -2','36612 -1','36612 -2','36613 -1','36613 -2','36614 -1','36614 -2'
 ];
 
 function shuffle(array: string[]) {
@@ -80,18 +22,47 @@ function shuffle(array: string[]) {
 }
 
 let listas: string[][] = [[],[],[]];
+let counterLista0deUnos = 0;
+let counterLista0deDoses = 0; 
+let counterLista1deUnos = 0;
+let counterLista1deDoses = 0; 
 
 function creaTresListas(arr: string[]): string[][] {
+
     arr.forEach((n: string)=> {
-        if(listas[0].findIndex(x => x.includes(n.substring(0,5))) == -1 && listas[0].length < listaCorreo.length /3) {
-            listas[0].push(n);
-        } else if (listas[1].findIndex(x => x.includes(n.substring(0,5))) == -1 && listas[1].length < listaCorreo.length/3) {
-            listas[1].push(n);
-        } else if (listas[2].length < listaCorreo.length/3) {
-            listas[2].push(n);
-        }
+        const fuk = [listas[0].findIndex(x => x.includes(n.substring(0,5)))==-1, listas[1].findIndex(x => x.includes(n.substring(0,5)))==-1, listas[2].findIndex(x => x.includes(n.substring(0,5)))==-1];
+        const fek = [listas[0].length < listas[1].length, listas[1].length < listaCorreo.length /3, listas[2].length < listaCorreo.length /3];
+
+        if (fuk[0] && fuk[1] && fuk[2]) {
+            if (listas[0].length < listas[1].length) {
+                listas[0].push(n)
+            } else if (listas[1].length < listas[2].length) {
+                listas[1].push(n)
+            } else {
+                listas[2].push(n)
+            }
+        }else if (fuk[0] && fuk[1] && !fuk[2]) {
+            if (listas[0].length < listas[1].length) {
+                listas[0].push(n)
+            } else {
+                listas[1].push(n)
+            }
+        } else if (!fuk[0] && fuk[1] && fuk[2]) {
+            if (listas[1].length < listas[2].length) {
+                listas[1].push(n)
+            } else {
+                listas[2].push(n)
+            }
+        }else 
+        if (fuk[0] && !fuk[1] && !fuk[2]) { 
+            listas[0].push(n); 
+        }  
+        else if (!fuk[0] && !fuk[1] && fuk[2]) listas[2].push(n)
+        else if (!fuk[0] && fuk[1] && !fuk[2]) listas[1].push(n)
+        else if (!fuk[0] && !fuk[1] && !fuk[2]) console.log('fuuuuuuuuuuuuuuuk')
+
     })
-    
+
     return listas;
 };
 
@@ -105,3 +76,12 @@ console.log('-1--------------------')
 listas[1].forEach(x => console.log(x))
 console.log('-2--------------------')
 listas[2].forEach(x => console.log(x))
+console.log('-DUP------------------')
+function findDuplicates(arr: string[]) {
+    let newArr = arr.map(x => x.substring(0,5))
+    return new Set(newArr).size !== newArr.length
+}
+
+console.log(findDuplicates(listas[0]))
+console.log(findDuplicates(listas[1]))
+console.log(findDuplicates(listas[2]))
